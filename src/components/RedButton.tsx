2882147cb9f5abbd22768ff468f0a53a7ddc6d2a
Nafis <nafisfragern@gmail.com>
2025-05-06 20:24:09 +0700

    [feat] button components

---
+++ src/components/RedButton.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+interface RedButtonProps {
+    caption: string;
+    href: string;
+}
+
+export default function RedButton({ caption, href }: RedButtonProps) {
+    return (
+        <Link href={href}>
+            <button className="bg-primary font-serif text-xl text-white py-4 px-14 rounded-bl-2xl rounded-tr-2xl rounded-tl-sm rounded-br-sm border border-primary shadow-[3px_3px_0px_0px_#662020]">
+                {caption}
+            </button>
+        </Link>
+    );
+}
---
+++ src/components/WhiteButton.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+interface WhiteButtonProps {
+    caption: string;
+    href: string;
+}
+
+export default function WhiteButton({ caption, href }: WhiteButtonProps) {
+    return (
+        <Link href={href}>
+            <button className="bg-white font-serif text-xl text-primary py-4 px-14 rounded-bl-2xl rounded-tr-2xl rounded-tl-sm rounded-br-sm border border-primary shadow-[3px_3px_0px_0px_#662020]">
+                {caption}
+            </button>
+        </Link>
+    );
+}
