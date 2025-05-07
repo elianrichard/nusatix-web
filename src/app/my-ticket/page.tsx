6790afc44dded43c98e7632285dc6b2061a7cbdb
Nafis <nafisfragern@gmail.com>
2025-05-07 22:26:21 +0700

    [feat] ticket responsivity

--- src/app/my-ticket/page.tsx
+++ src/app/my-ticket/page.tsx
@@ -1,11 +1,39 @@
-import TicketCard from "@/components/ticketCard";
+// app/my-ticket/page.tsx
+"use client";
 
+import TicketCard from "@/components/ticketCard";
+import type { TicketType } from "@/components/ticketCard";
+import { useState } from "react";
+import Modal from "@/components/Modal";
+import Image from "next/image";
 
 export default function MyTicket() {
+    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedTicket, setTicketType] = useState<TicketType | null>(null);
+
+    const handleCardClick = (ticket: TicketType) => {
+        setTicketType(selectedTicket);
+        setModalOpen(true);
+    };
+
+    const ticket: TicketType = {
+        title: "Tari Kecak Bali",
+        location: "Jl. Bali No. 20, Bali, Provinsi Bali 12345",
+        date: "June 24, 2024 | 08.00 PM",
+    };
+
     return (
-        <div className="flex flex-col p-16">
-            <h1 className="font-serif text-primary text-6xl font-bold py-8">Active Tickets</h1>
-            <TicketCard/>
+        <div className="flex flex-col p-5 md:p-16">
+            <h1 className="font-serif text-primary text-2xl md:text-6xl font-bold py-4 md:py-8">
+                Active Tickets
+            </h1>
+
+            <TicketCard type={ticket} onClick={() => handleCardClick(ticket)} />
+
+            <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+                <h1 className="text-2xl md:text-3xl text-center text-primary">Your Ticket</h1>
+                <Image src={"/mockup-QR.png"} alt="QR Code" width={371} height={371} className="w-full mx-auto pt-10" />
+            </Modal>
         </div>
-    ) 
+    );
 }
--- src/components/ticketCard.tsx
+++ src/components/ticketCard.tsx
@@ -1,16 +1,41 @@
 import Image from "next/image";
 
+export type TicketType = {
+  title: string;
+  location: string;
+  date: string;
+};
 
-export default function TicketCard() {
-    return (
-        <div className="flex flex-row justify-between p-10 bg-primary rounded-2xl w-full my-8">
-            <div className="flex flex-col justify-center">
-                <h1 className="text-white font-serif text-5xl">Tari Kecak Bali</h1>
-                <h2 className="text-white opacity-70 font-sans text-2xl mt-2">Jl. Bali No. 20, Bali, Provinsi Bali 12345</h2>
-                <p className="text-white text-2xl mt-8">June 24, 2024 | 08.00 PM</p>
-            </div>
-            
-            <Image src={"/qr-code.svg"} alt="test" width={125} height={125} />
-        </div>
-    )
+type Props = {
+  type: TicketType;
+  onClick?: () => void;
+};
+
+export default function TicketCard({ type, onClick }: Props) {
+  return (
+    <div
+      onClick={onClick}
+      className="group cursor-pointer flex flex-col md:flex-row justify-between p-5 md:p-10 bg-primary rounded-2xl w-full my-4 md:my-8 border border-primary hover:bg-white transition duration-300 ease-in-out"
+    >
+      <div className="flex flex-col justify-center">
+        <h1 className="text-white font-serif text-3xl font-bold md:text-5xl group-hover:text-primary transition-colors duration-300">
+          {type.title}
+        </h1>
+        <h2 className="text-white opacity-70 font-sans font-light text-base md:text-2xl mt-2 group-hover:text-primary transition-colors duration-300">
+          {type.location}
+        </h2>
+        <p className="text-white text-base md:text-2xl font-light mt-8 group-hover:text-primary transition-colors duration-300">
+          {type.date}
+        </p>
+      </div>
+
+      <Image
+        src="/qr-code.svg"
+        alt="QR Code"
+        width={125}
+        height={125}
+        className="w-20 md:w-32 h-auto mt-16 md:mt-0"
+      />
+    </div>
+  );
 }
