7af62d668e88779deddeaf26e6ad2c9a200fca7e
Nafis <nafisfragern@gmail.com>
2025-05-07 22:25:31 +0700

    [feat] button responsivity

--- src/components/RedButton.tsx
+++ src/components/RedButton.tsx
@@ -8,7 +8,7 @@ interface RedButtonProps {
 export default function RedButton({ caption, href }: RedButtonProps) {
     return (
         <Link href={href}>
-            <button className="bg-primary font-serif text-xl text-white py-4 px-14 rounded-bl-2xl rounded-tr-2xl rounded-tl-sm rounded-br-sm border border-primary shadow-[3px_3px_0px_0px_#662020]">
+            <button className="bg-primary font-serif text-lg md:text-xl text-white py-2 md:py-4 px-7 md:px-14 rounded-bl-2xl rounded-tr-2xl rounded-tl-sm rounded-br-sm border border-primary shadow-[3px_3px_0px_0px_#662020]">
                 {caption}
             </button>
         </Link>
